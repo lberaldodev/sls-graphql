0a29cc84126dcec52040e590167c0a2f37de074b
LucasBeraldo <45186203+lberaldodev@users.noreply.github.com>
2020-08-30 16:12:43 -0300

    fix: example request to use with core

--- handler.js
+++ handler.js
@@ -1,41 +1,93 @@
 "use strict";
+const uuid = require("uuid");
+const setupDynamoDB = require("./src/core/util/setupDynamoDB");
 
-const AWS = require("aws-sdk");
+setupDynamoDB();
 
-function setupDynamoDB() {
-  if (!process.env.IS_LOCAL) return new AWS.DynamoDB.DocumentClient();
-  
-	const host = process.env.LOCALSTACK_HOST;
-	const port = process.env.DYNAMODB_PORT;
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const HeroFactory = require("./src/core/factories/heroFactory");
+const SkillsFactory = require("./src/core/factories/skillsFactory");
 
-  console.log("running local!", host, port);
-  
-  return new AWS.DynamoDB.DocumentClient({
-    region: "localhost",
-    accessKeyId: "DEFAULT_ACCESS_KEY",
-    secretAccessKey: "DEFAULT_SECRET",
-    endpoint: new AWS.Endpoint(`http://${host}:${port}`)
-  })
-}
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+	type Query {
+		hello: String
+	}
+`;
 
-module.exports.hello = async (event) => {
-  const dynamodb = setupDynamoDB();
-  
-  const heroes = await dynamodb.scan({
-    TableName: process.env.HEROES_TABLE
-  }).promise()
+// Provide resolver functions for your schema fields
+const resolvers = {
+	Query: {
+		hello: () => "Hello world!",
+	},
+};
 
-  const skills = await dynamodb.scan({
-    TableName: process.env.SKILLS_TABLE
-  }).promise()
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+exports.handler = server.createHandler({
+	cors: {
+		origin: "*",
+		credentials: true,
+	},
+});
+
+async function main() {
+	console.log("creating factories");
+
+	const skillFactory = await SkillsFactory.createInstance();
+
+	const heroFactory = await HeroFactory.createInstance();
+
+	const skill_id = uuid.v1();
+
+	await skillFactory.create({
+		id: skill_id,
+		name: "mage",
+		value: 5,
+	});
+
+	console.log("get last skill inserted");
+
+	const skill = await skillFactory.findOne(skill_id);
+
+	console.log(skill);
+
+	console.log("get all skills");
+
+	const skills = await skillFactory.findAll();
+
+	console.log(skills);
+
+	const hero_id = uuid.v1();
+
+	await heroFactory.create({
+		id: hero_id,
+		name: "batman",
+		skills: [skill_id],
+	});
+
+	console.log("get last hero inserted");
+
+	const hero = await heroFactory.findOne(hero_id);
+
+	console.log(hero);
+
+	console.log("get all heros");
+
+	const heros = await heroFactory.findAll();
+
+	console.log(heros);
 
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-      heroes, skills
-    }),
+			heros,
+			skills,
+		}),
 	};
+}
 
-	// Use this code if you don't use the http event with the LAMBDA-PROXY integration
-	// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.main = main;
